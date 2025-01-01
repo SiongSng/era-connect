904c775ec86556f255ed43c414c68cef3651c3dd
KyleUltimate <shiue.kyle@gmail.com>
2025-01-01 08:45:34 +0800

    refactor: remove flutter related stuff

--- app/lib/api/lib.dart
+++
@@ -1,4 +0,0 @@
-export 'storage/lib.dart';
-export 'authentication/lib.dart';
-export 'meta/lib.dart';
-export 'collection/collection_api.dart';
--- app/lib/api/meta/lib.dart
+++
@@ -1,3 +0,0 @@
-export 'meta_api.dart';
-export 'package:era_connect/src/rust/api/backend_exclusive/vanilla/version.dart'
-    show VersionType, VersionMetadata;
